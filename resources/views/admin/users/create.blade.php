436d7c8e31298c5d7355bce1b7aa0a6b97302a6d
Lasha Palelashvili <falelashvili@gmail.com>
2018-06-26 18:54:26 +0400

    Added inputs and custom request to create page

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -8,13 +8,35 @@
 
     {!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store']) !!}
             <div class="form-group">
-                {!! Form::label('title', 'Title:') !!}
-                {!! Form::text('title', null, ['class'=>'form-control']) !!}
+                {!! Form::label('name', 'Name:') !!}
+                {!! Form::text('name', null, ['class'=>'form-control']) !!}
             </div>
 
             <div class="form-group">
-                {!! Form::submit('Create Post', ['class'=>'btn btn-primary']) !!}
+                {!! Form::label('email', 'Email:') !!}
+                {!! Form::email('email', null, ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('status', 'Status:') !!}
+                {!! Form::select('status', array(1=>'Active', 0=>'Not Active'), null, ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('role_id', 'Role:') !!}
+                {!! Form::select('role_id', array(''=>'Choose options')+$roles, null, ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('password', 'Password:') !!}
+                {!! Form::password('password', ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
             </div>
     {!! Form::close() !!}
+
+    @include('includes.form_error')
 
 @stop
