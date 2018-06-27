6bab890958e0c0457f12b3f96c33f55b471d4efb
Lasha Palelashvili <falelashvili@gmail.com>
2018-06-27 15:48:49 +0400

    Added photo upload field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->integer('role_id')->index()->unsigned();
             $table->integer('is_active')->default(0);
+            $table->string('photo_id');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
